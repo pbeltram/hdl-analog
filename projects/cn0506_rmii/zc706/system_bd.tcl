msporcich/hdl

projects/cn0506_rmii/*: Add mii_to_rmii_ad library to project

projects/cn0506_rmii/zc706/system_bd.tcl
@@ -19,37 +19,35 @@ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:rmii_rtl:1.0 RMII_PH
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps7/MDIO_ETHERNET_0]
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps7/MDIO_ETHERNET_1]
 
-ad_ip_instance mii_to_rmii mii_to_rmii_0
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance mii_to_rmii_ad mii_to_rmii_0
+ad_ip_parameter mii_to_rmii_0 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_0 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_0/GMII    sys_ps7/GMII_ETHERNET_0
 ad_connect mii_to_rmii_0/ref_clk ref_clk_50_a
 
-ad_connect mii_to_rmii_0/RMII_PHY_M RMII_PHY_M_0
+ad_connect mii_to_rmii_0/RMII RMII_PHY_M_0
 
-ad_ip_instance mii_to_rmii mii_to_rmii_1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance mii_to_rmii_ad mii_to_rmii_1
+ad_ip_parameter mii_to_rmii_1 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_1 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_1/GMII    sys_ps7/GMII_ETHERNET_1
 ad_connect mii_to_rmii_1/ref_clk ref_clk_50_b
 
-ad_connect mii_to_rmii_1/RMII_PHY_M RMII_PHY_M_1
+ad_connect mii_to_rmii_1/RMII RMII_PHY_M_1
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth0
 ad_connect proc_sys_reset_eth0/slowest_sync_clk  ref_clk_50_a
 ad_connect proc_sys_reset_eth0/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth0/peripheral_reset  reset_a
-ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/rst_n
+ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/reset_n
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth1
 ad_connect proc_sys_reset_eth1/slowest_sync_clk  ref_clk_50_b
 ad_connect proc_sys_reset_eth1/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth1/peripheral_reset  reset_b
-ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/rst_n
+ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/reset_n
 
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 

projects/cn0506_rmii/zcu102/system_bd.tcl
@@ -24,37 +24,35 @@ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:rmii_rtl:1.0 RMII_PH
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps8/MDIO_ENET0]
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps8/MDIO_ENET1]
 
-ad_ip_instance mii_to_rmii mii_to_rmii_0
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance mii_to_rmii_ad mii_to_rmii_0
+ad_ip_parameter mii_to_rmii_0 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_0 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_0/GMII    sys_ps8/GMII_ENET0
 ad_connect mii_to_rmii_0/ref_clk ref_clk_50_a
 
-ad_connect mii_to_rmii_0/RMII_PHY_M RMII_PHY_M_0
+ad_connect mii_to_rmii_0/RMII RMII_PHY_M_0
 
-ad_ip_instance mii_to_rmii mii_to_rmii_1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance mii_to_rmii_ad mii_to_rmii_1
+ad_ip_parameter mii_to_rmii_1 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_1 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_1/GMII    sys_ps8/GMII_ENET1
 ad_connect mii_to_rmii_1/ref_clk ref_clk_50_b
 
-ad_connect mii_to_rmii_1/RMII_PHY_M RMII_PHY_M_1
+ad_connect mii_to_rmii_1/RMII RMII_PHY_M_1
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth0
 ad_connect proc_sys_reset_eth0/slowest_sync_clk  ref_clk_50_a
 ad_connect proc_sys_reset_eth0/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth0/peripheral_reset  reset_a
-ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/rst_n
+ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/reset_n
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth1
 ad_connect proc_sys_reset_eth1/slowest_sync_clk  ref_clk_50_b
 ad_connect proc_sys_reset_eth1/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth1/peripheral_reset  reset_b
-ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/rst_n
+ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/reset_n
 
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 

projects/cn0506_rmii/zed/system_bd.tcl
@@ -18,37 +18,35 @@ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:rmii_rtl:1.0 RMII_PH
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps7/MDIO_ETHERNET_0]
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps7/MDIO_ETHERNET_1]
 
-ad_ip_instance mii_to_rmii mii_to_rmii_0
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance mii_to_rmii_ad mii_to_rmii_0
+ad_ip_parameter mii_to_rmii_0 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_0 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_0/GMII    sys_ps7/GMII_ETHERNET_0
 ad_connect mii_to_rmii_0/ref_clk ref_clk_50_a
 
-ad_connect mii_to_rmii_0/RMII_PHY_M RMII_PHY_M_0
+ad_connect mii_to_rmii_0/RMII RMII_PHY_M_0
 
-ad_ip_instance mii_to_rmii mii_to_rmii_1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance mii_to_rmii_ad mii_to_rmii_1
+ad_ip_parameter mii_to_rmii_1 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_1 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_1/GMII    sys_ps7/GMII_ETHERNET_1
 ad_connect mii_to_rmii_1/ref_clk ref_clk_50_b
 
-ad_connect mii_to_rmii_1/RMII_PHY_M RMII_PHY_M_1
+ad_connect mii_to_rmii_1/RMII RMII_PHY_M_1
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth0
 ad_connect proc_sys_reset_eth0/slowest_sync_clk  ref_clk_50_a
 ad_connect proc_sys_reset_eth0/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth0/peripheral_reset  reset_a
-ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/rst_n
+ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/reset_n
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth1
 ad_connect proc_sys_reset_eth1/slowest_sync_clk  ref_clk_50_b
 ad_connect proc_sys_reset_eth1/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth1/peripheral_reset  reset_b
-ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/rst_n
+ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/reset_n
 
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 

pbeltram/hdl-analog

projects/cn0506_rmii/zc706/system_bd.tcl
@@ -19,37 +19,35 @@ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:rmii_rtl:1.0 RMII_PH
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps7/MDIO_ETHERNET_0]
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps7/MDIO_ETHERNET_1]
 
-ad_ip_instance mii_to_rmii mii_to_rmii_0
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance util_mii_to_rmii mii_to_rmii_0
+ad_ip_parameter mii_to_rmii_0 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_0 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_0/GMII    sys_ps7/GMII_ETHERNET_0
 ad_connect mii_to_rmii_0/ref_clk ref_clk_50_a
 
-ad_connect mii_to_rmii_0/RMII_PHY_M RMII_PHY_M_0
+ad_connect mii_to_rmii_0/RMII RMII_PHY_M_0
 
-ad_ip_instance mii_to_rmii mii_to_rmii_1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance util_mii_to_rmii mii_to_rmii_1
+ad_ip_parameter mii_to_rmii_1 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_1 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_1/GMII    sys_ps7/GMII_ETHERNET_1
 ad_connect mii_to_rmii_1/ref_clk ref_clk_50_b
 
-ad_connect mii_to_rmii_1/RMII_PHY_M RMII_PHY_M_1
+ad_connect mii_to_rmii_1/RMII RMII_PHY_M_1
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth0
 ad_connect proc_sys_reset_eth0/slowest_sync_clk  ref_clk_50_a
 ad_connect proc_sys_reset_eth0/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth0/peripheral_reset  reset_a
-ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/rst_n
+ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/reset_n
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth1
 ad_connect proc_sys_reset_eth1/slowest_sync_clk  ref_clk_50_b
 ad_connect proc_sys_reset_eth1/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth1/peripheral_reset  reset_b
-ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/rst_n
+ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/reset_n
 
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 

projects/cn0506_rmii/zcu102/system_bd.tcl
@@ -24,37 +24,35 @@ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:rmii_rtl:1.0 RMII_PH
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps8/MDIO_ENET0]
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps8/MDIO_ENET1]
 
-ad_ip_instance mii_to_rmii mii_to_rmii_0
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance util_mii_to_rmii mii_to_rmii_0
+ad_ip_parameter mii_to_rmii_0 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_0 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_0/GMII    sys_ps8/GMII_ENET0
 ad_connect mii_to_rmii_0/ref_clk ref_clk_50_a
 
-ad_connect mii_to_rmii_0/RMII_PHY_M RMII_PHY_M_0
+ad_connect mii_to_rmii_0/RMII RMII_PHY_M_0
 
-ad_ip_instance mii_to_rmii mii_to_rmii_1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance util_mii_to_rmii mii_to_rmii_1
+ad_ip_parameter mii_to_rmii_1 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_1 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_1/GMII    sys_ps8/GMII_ENET1
 ad_connect mii_to_rmii_1/ref_clk ref_clk_50_b
 
-ad_connect mii_to_rmii_1/RMII_PHY_M RMII_PHY_M_1
+ad_connect mii_to_rmii_1/RMII RMII_PHY_M_1
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth0
 ad_connect proc_sys_reset_eth0/slowest_sync_clk  ref_clk_50_a
 ad_connect proc_sys_reset_eth0/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth0/peripheral_reset  reset_a
-ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/rst_n
+ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/reset_n
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth1
 ad_connect proc_sys_reset_eth1/slowest_sync_clk  ref_clk_50_b
 ad_connect proc_sys_reset_eth1/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth1/peripheral_reset  reset_b
-ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/rst_n
+ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/reset_n
 
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 

projects/cn0506_rmii/zed/system_bd.tcl
@@ -18,37 +18,35 @@ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:rmii_rtl:1.0 RMII_PH
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps7/MDIO_ETHERNET_0]
 make_bd_intf_pins_external  [get_bd_intf_pins sys_ps7/MDIO_ETHERNET_1]
 
-ad_ip_instance mii_to_rmii mii_to_rmii_0
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_0 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance util_mii_to_rmii mii_to_rmii_0
+ad_ip_parameter mii_to_rmii_0 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_0 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_0/GMII    sys_ps7/GMII_ETHERNET_0
 ad_connect mii_to_rmii_0/ref_clk ref_clk_50_a
 
-ad_connect mii_to_rmii_0/RMII_PHY_M RMII_PHY_M_0
+ad_connect mii_to_rmii_0/RMII RMII_PHY_M_0
 
-ad_ip_instance mii_to_rmii mii_to_rmii_1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_MODE 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_SPEED_100 1
-ad_ip_parameter mii_to_rmii_1 CONFIG.C_FIXED_SPEED 0
+ad_ip_instance util_mii_to_rmii mii_to_rmii_1
+ad_ip_parameter mii_to_rmii_1 CONFIG.INTF_CFG 1
+ad_ip_parameter mii_to_rmii_1 CONFIG.RATE_10_100 0
 
 ad_connect mii_to_rmii_1/GMII    sys_ps7/GMII_ETHERNET_1
 ad_connect mii_to_rmii_1/ref_clk ref_clk_50_b
 
-ad_connect mii_to_rmii_1/RMII_PHY_M RMII_PHY_M_1
+ad_connect mii_to_rmii_1/RMII RMII_PHY_M_1
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth0
 ad_connect proc_sys_reset_eth0/slowest_sync_clk  ref_clk_50_a
 ad_connect proc_sys_reset_eth0/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth0/peripheral_reset  reset_a
-ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/rst_n
+ad_connect proc_sys_reset_eth0/peripheral_aresetn  mii_to_rmii_0/reset_n
 
 ad_ip_instance proc_sys_reset proc_sys_reset_eth1
 ad_connect proc_sys_reset_eth1/slowest_sync_clk  ref_clk_50_b
 ad_connect proc_sys_reset_eth1/ext_reset_in  sys_rstgen/peripheral_aresetn
 ad_connect proc_sys_reset_eth1/peripheral_reset  reset_b
-ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/rst_n
+ad_connect proc_sys_reset_eth1/peripheral_aresetn  mii_to_rmii_1/reset_n
 
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 