5cec9e913c2ee80ea6006aef9749ea1fbcf35baf
pbeltram <primoz.beltram@gmail.com>
2021-12-12 15:09:50 +0100

    util_upack: Add IP project files from analog-hdl 2016.4.

---
+++ library/util_pack/util_upack/util_upack_ip.tcl
@@ -0,0 +1,58 @@
+# ***************************************************************************
+# ***************************************************************************
+# Copyright 2018 (c) Analog Devices, Inc. All rights reserved.
+#
+# Each core or library found in this collection may have its own licensing terms.
+# The user should keep this in in mind while exploring these cores.
+#
+# Redistribution and use in source and binary forms,
+# with or without modification of this file, are permitted under the terms of either
+#  (at the option of the user):
+#
+#   1. The GNU General Public License version 2 as published by the
+#      Free Software Foundation, which can be found in the top level directory, or at:
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+#
+# OR
+#
+#   2.  An ADI specific BSD license as noted in the top level directory, or on-line at:
+# https://github.com/analogdevicesinc/hdl/blob/dev/LICENSE
+#
+# ***************************************************************************
+# ***************************************************************************
+
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
+
+adi_ip_create util_upack
+adi_ip_files util_upack [list \
+  "util_upack_dmx.v" \
+  "util_upack_dsf.v" \
+  "util_upack.v" \
+  "util_upack_constr.xdc" ]
+
+adi_ip_properties_lite util_upack
+
+set_property driver_value 0 [ipx::get_ports *dac_enable* -of_objects [ipx::current_core]]
+set_property driver_value 0 [ipx::get_ports *dac_valid* -of_objects [ipx::current_core]]
+set_property driver_value 0 [ipx::get_ports *dac_data* -of_objects [ipx::current_core]]
+set_property driver_value 0 [ipx::get_ports *dma_xfer_in* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 1} \
+  [ipx::get_ports *_1* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 2} \
+  [ipx::get_ports *_2* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 3} \
+  [ipx::get_ports *_3* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 4} \
+  [ipx::get_ports *_4* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 5} \
+  [ipx::get_ports *_5* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 6} \
+  [ipx::get_ports *_6* -of_objects [ipx::current_core]]
+set_property enablement_dependency {spirit:decode(id('MODELPARAM_VALUE.NUM_OF_CHANNELS')) > 7} \
+  [ipx::get_ports *_7* -of_objects [ipx::current_core]]
+
+ipx::remove_all_bus_interface [ipx::current_core]
+ipx::save_core [ipx::current_core]
+
+
