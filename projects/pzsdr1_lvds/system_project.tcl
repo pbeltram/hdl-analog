dcdc38609ed13f0718d6dad8d6c3baa0a98946fc
pbeltram <primoz.beltram@gmail.com>
2021-12-22 18:12:38 +0100

    pzsdr1_lvds: Rename project files.

--- projects/pzsdr1_lvds/system_project.tcl
+++ projects/pzsdr1_lvds/system_project.tcl
@@ -3,17 +3,15 @@ source ../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-adi_project_create pzsdr_lvds 0 {} "xc7z020clg400-1"
+adi_project_create pzsdr1_lvds 0 {} "xc7z020clg400-1"
 
-adi_project_files pzsdr_lvds [list \
+adi_project_files pzsdr1_lvds [list \
   "system_top.v" \
-  "pzsdr1_constr.xdc" \
   "pzsdr1_constr_lvds.xdc" \
-  "ccbrk_constr.xdc" \
   "$ad_hdl_dir/library/common/ad_iobuf.v"]
 
-set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
-set_property is_enabled false [get_files  *axi_gpreg_0_constr.xdc]
+#set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
+#set_property is_enabled false [get_files  *axi_gpreg_0_constr.xdc]
 adi_project_run pzsdr1_lvds
 source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
 
