a132a6a6173fe69f59016cee59a9e0fc999ea929
pbeltram <primoz.beltram@gmail.com>
2021-12-22 18:17:02 +0100

    pzsdr1_lvds: Rename project files.

--- projects/pzsdr1_lvds/system_project.tcl
+++ projects/pzsdr1_lvds/system_project.tcl
@@ -7,12 +7,14 @@ adi_project_create pzsdr1_lvds 0 {} "xc7z020clg400-1"
 
 adi_project_files pzsdr1_lvds [list \
   "system_top.v" \
-  "pzsdr1_constr_lvds.xdc" \
+  "pzsdr1_lvds.xdc" \
+  "pzsdr1_lvds_pins.xdc" \
   "$ad_hdl_dir/library/common/ad_iobuf.v"]
 
 #set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
 #set_property is_enabled false [get_files  *axi_gpreg_0_constr.xdc]
 adi_project_run pzsdr1_lvds
+
 source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
 
 
